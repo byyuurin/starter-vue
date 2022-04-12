d5fbeefdb60427c8e1a2fd33812d9b2923c2b5ec
Yuurin <byyuurin@gmail.com>
2022-04-12 18:48:30 +0800

    chore: update [skip ci]

--- src/utils/unocss.ts
+++
@@ -1,6 +0,0 @@
-export function createMixins<O extends Record<string, string>>(options: O) {
-  return <K extends keyof O>(name: K, appends: string) => {
-    const splitter = /\s/
-    return [...options[name].split(splitter), ...appends.split(splitter)].join(' ')
-  }
-}

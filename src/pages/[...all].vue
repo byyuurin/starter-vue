d4ba4fff5a069d583508de61707b0950c9f48475
Yuurin <byyuurin@gmail.com>
2022-07-31 12:10:58 +0800

    chore: update

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -3,8 +3,19 @@ meta:
   title: Page Not Found
 </route>
 
+<script setup lang="ts">
+const router = useRouter()
+</script>
+
 <template>
-  <div h="600px" max-h="75vh" flex justify-center items-center>
-    <span text-4xl>Page Not Found</span>
+  <div absolute min-w-full min-h-full bg-base flex justify-center items-center select-none>
+    <div px-5 py-10 flex="~ col gap-4" justify-center items-center>
+      <i text-8xl i-twemoji-crying-face></i>
+      <span text-4xl>Page Not Found</span>
+
+      <button btn="~ base" @click="router.replace({ path: '/' })">
+        Back
+      </button>
+    </div>
   </div>
 </template>

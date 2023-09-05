c9bf06c8a6345d16187d01ec569c284943ae8cd7
byyuurin <byyuurin@gmail.com>
2023-09-05 11:46:21 +0000

    Deploying to gh-pages from @ byyuurin/starter-vue@c3303fb5a8c460d125cc168e91adb32a87f054a8 🚀

---
+++ about.9c07b052.js
@@ -0,0 +1,1 @@
+import{d as e,u as t,o,c as a}from"./assets/index-493751d1.js";const r=e({__name:"about",setup(n){return t({title:"About"}),(s,u)=>(o(),a("div",null,"About page"))}});export{r as default};
---
+++ id.64472f16.js
@@ -0,0 +1,1 @@
+import{d as o,u as s,o as a,c as r,t as i}from"./assets/index-493751d1.js";const n={class:"p-4"},m=o({__name:"[id]",props:{id:{}},setup(e){const t=e;return s({title:"Dynamic Page with `props.id` or `route.params.id`"}),(p,c)=>(a(),r("div",n,i(t),1))}});export{m as default};

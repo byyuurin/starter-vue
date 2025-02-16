9663a2dc50e04ad0d1e9b2e96e988ff02710874f
byyuurin <byyuurin@users.noreply.github.com>
2025-02-16 14:27:14 +0000

    deploy: 4ed28eb7ee4048d20ca598eba4d95affbaf46ea2

---
+++ about.DAwuSu-6.js
@@ -0,0 +1,1 @@
+import{d as e,h as t,a as o,o as a}from"./assets/index-B97GOKTe.js";const c=e({__name:"about",setup(n){return t({title:"About"}),(s,r)=>(a(),o("div",null,"About page"))}});export{c as default};
